d4d2ac52095b3043f48b365796cbf1043fe5e5fb
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-20 22:41:43 +0530

    Add files via upload

---
+++ set_add_hr.py
@@ -0,0 +1,17 @@
+"""set.add() problem using Python(HackerRank)"""
+
+'''method 1'''
+# N = int(input())
+# countries = set()
+# for i in range(N):
+#     countries.add(input())
+# print(len(countries))
+
+
+'''method 2'''
+# total_stamps = int(input())
+# countrynames = set()
+# for i in range(total_stamps):
+#     singlecountry = str(input())
+#     countrynames.add(singlecountry)
+# print(len(countrynames))
